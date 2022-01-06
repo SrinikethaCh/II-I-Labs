9cf000b33ca14c209cda02c8033c2c0512d0bc46
SrinikethaCh <95291279+SrinikethaCh@users.noreply.github.com>
2022-01-06 17:56:57 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 4/Student_struct.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+struct student{
+	string Name;
+	int roll_no;
+	string branch;
+	char grade;
+}s;
+
+int main(){
+	cout<<"Enter the Name: ";
+	cin>>s.Name;
+	cout<<"Enter the Roll.No: ";
+	cin>>s.roll_no;
+	cout<<"Enter the Branch: ";
+	cin>>s.branch;
+	cout<<"Enter the Grade: ";
+	cin>>s.grade;
+
+	cout<<"Name of the student is : "<<s.Name<<endl;
+	cout<<"Roll.No of the student is : "<<s.roll_no<<endl;
+	cout<<"Branch : "<<s.branch<<endl;
+	cout<<"Grade : "<<s.grade<<endl;
+	
+	return 0;
+}
