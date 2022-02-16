f64e84e1708ab5c3e485a78686b295afd377c395
SrinikethaCh <95291279+SrinikethaCh@users.noreply.github.com>
2022-02-16 19:30:06 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 12/12B/method_overriding.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class parent{
+        public:
+        void display(){
+                cout<<"Parent Class"<<endl;
+        }
+};
+
+class child:public parent{
+        public:
+        void display(){
+                cout<<"Child Class"<<endl;
+        }
+};
+
+int main(){
+        child c;
+        c.display();
+        return 0;
+}
